509f4783d171de86cf48be59603dfb4c92d04cce
Justin Shieh <jshieh@atlassian.com>
2024-05-14 18:52:04 -0700

    fix tests

--- source/DruidXbasicExtension/src/test/java/com/amazon/solutions/druid/xbasic/XbasicAuthenticatorTest.java
+++ source/DruidXbasicExtension/src/test/java/com/amazon/solutions/druid/xbasic/XbasicAuthenticatorTest.java
@@ -1,15 +1,19 @@
 package com.amazon.solutions.druid.xbasic;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 import javax.servlet.Filter;
 import org.junit.Test;
 
 public class XbasicAuthenticatorTest {
     private XbasicAuthenticator authenticator;
+    private XbasicConfig xbasicConfig;
+
     @Test
     public void canInitialiseOidcFilter() {
-        authenticator = new XbasicAuthenticator();
+        xbasicConfig = mock(XbasicConfig.class);
+        authenticator = new XbasicAuthenticator("name", "authorizerName", xbasicConfig);
         Filter oidcFilter = authenticator.getFilter();
 
         assertNotNull(oidcFilter);
